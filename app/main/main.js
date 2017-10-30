cf4c209006bef9c03468c9e447a053ea8f457709
Samuel Dubner <dubnersamuel@yahoo.com>
2017-10-30 14:52:30 -0700

    icon

--- app/main/main.js
+++ app/main/main.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const iconExtractor = require('icon-extractor');
 
 addGame = (file) => {
   var gameDiv = $(document.createElement('div'));
@@ -11,13 +12,40 @@ addGame = (file) => {
   console.log(file);
 }
 
-const steam = "C:/Program Files (x86)/Steam/steamapps/common";
-const origin = "C:/Program Files (x86)/Origin Games";
+const steam = "C:/Program Files (x86)/Steam/steamapps/common/";
+const origin = "C:/Program Files (x86)/Origin Games/";
+
+var pictureName;
 
 fs.readdirSync(steam).forEach(file => {
-  addGame(file);
+  fs.readdirSync(steam + file).forEach(picture => {
+    if (picture.substring(picture.length - 3, picture.length) == '.exe') {
+      console.log(dir + picture + '.exe');
+      pictureName = picture;
+      iconExtractor.getIcon(picture, dir + picture + '.exe');
+    }
+  })
+  addGame(file, picture);
 })
 
 fs.readdirSync(origin).forEach(file => {
-  addGame(file);
+  fs.readdirSync(dir).forEach(picture => {
+    if (picture.substring(picture.length - 3, picture.length) == '.exe') {
+      console.log(dir + picture + '.exe');
+      pictureName = picture;
+      iconExtractor.getIcon(picture, dir + picture + '.exe');
+    }
+  })
+  addGame(file, picture);
 })
+
+iconExtractor.emitter.on('icon', function (data) {
+    console.log('Here is my context: ' + data.Context);
+    console.log('Here is the path it was for: ' + data.Path);
+    var icon = data.Base64ImageData;
+
+    fs.writeFile(file + '.png', icon, 'base64', (err) => {
+        console.log(err);
+    });
+
+});
