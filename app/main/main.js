1956c948b5dc2b5797c9b080e8f5ad2d206c007f
Samuel Dubner <dubnersamuel@yahoo.com>
2017-11-02 09:09:41 -0700

    comment everything

--- app/main/main.js
+++ app/main/main.js
@@ -1,62 +1,87 @@
+//get icon
 const remote = require('electron').remote;
-const fs = require('fs');
-const child = require('child_process').execFile;
 const app = remote.app;
 
+//create .png files
+const fs = require('fs');
+
+//run .exe
+const child = require('child_process').execFile;
+
+//add game to container
 addGame = (file, gameIcons) => {
+
+  //div that holds all the content
   var gameDiv = $(document.createElement('div'));
-  var gameIcon = $(document.createElement('img'));
-  var gameTitle = $(document.createElement('h3'));
-  var playButton = $(document.createElement('input'));
-  //<input type="button" class="buttons" id="start" value="Play Game"></input>
   gameDiv.addClass('game');
+
+  //icon next to the play button
+  var gameIcon = $(document.createElement('img'));
   gameIcon.addClass('game-icon');
+  var iconPath = app.getPath("appData") + "/gm/icons/" + gameIcons[0].substring(0, gameIcons[0].length - 4);
+  gameIcon.attr('src', iconPath + ".png");
+
+  var gameTitle = $(document.createElement('h3'));
   gameTitle.addClass('game-title');
-  playButton.addClass('buttons');
   if (file == "PUBG/TslGame/Binaries/Win64") {
     gameTitle.html("PUBG");
   } else {
     gameTitle.html(file);
   }
-  console.log(gameIcons);
-  var iconPath = app.getPath("appData") + "/gm/icons/" + gameIcons[0].substring(0, gameIcons[0].length - 4);
+
+  //button to launch the game
+  var playButton = $(document.createElement('input'));
+  playButton.addClass('buttons');
   playButton.val('Play Game');
-  gameIcon.attr('src', iconPath + ".png");
   playButton.attr('type', 'button')
   playButton.attr('id', gameIcons[0]);
   playButton.attr('gameName', file);
+
+  //append all elements to the div and to the container
   $('.game-container').append(gameDiv);
   $(gameDiv).append(gameIcon);
   $(gameDiv).append(gameTitle);
   $(gameDiv).append(playButton);
+
 }
 
+//command that runs executable
 function runExe(executablePath) {
+
   child(executablePath, function (err, data) {
+
     if (err) {
       console.error(err);
       return;
     }
 
     console.log(data.toString());
+
   });
 }
 
+//get path for most popular game clients
 const steam = "C:/Program Files (x86)/Steam/steamapps/common/";
 const origin = "C:/Program Files (x86)/Origin Games/";
 
+//create object to hold array of icon names
 let gameIcons = {};
 
+//create folder to hold icons if it doesn't exist
 if (!fs.existsSync(app.getPath("appData") + "/gm/icons/")) {
   fs.mkdirSync(app.getPath("appData") + "/gm/icons/");
 }
 
+//loops through each folder in the steam directory
 fs.readdirSync(steam).forEach(file => {
+
   let pictureName;
+  //why PUBG ;-;
   if (file == "PUBG") {
     file = file + "/TslGame/Binaries/Win64";
   }
   gameIcons[file] = [];
+  //loop through every .exe in every game folder
   fs.readdirSync(steam + file).forEach(picture => {
     if (picture.substring(picture.length - 4, picture.length) == '.exe') {
       pictureName = picture;
@@ -73,7 +98,9 @@ fs.readdirSync(steam).forEach(file => {
   })
 })
 
+//loops through each folder in the origin directory
 fs.readdirSync(origin).forEach(file => {
+
   let pictureName;
   gameIcons[file] = [];
   fs.readdirSync(origin + file).forEach(picture => {
@@ -92,13 +119,15 @@ fs.readdirSync(origin).forEach(file => {
   })
 })
 
-
+//adds game for every key (game) in the object
 for (let game in gameIcons) {
   if (gameIcons[game].length != 0) {
     addGame(game, gameIcons[game]);
   }
 }
 
+
+//search funtion to look through all the games
 $("#search-text").on("keyup", function () {
   var g = $(this).val().toLowerCase();
   $(".game .game-title").each(function () {
@@ -108,6 +137,7 @@ $("#search-text").on("keyup", function () {
 
 })
 
+//runs .exe when play button is clicked
 $(document).on('click', '.buttons', function () {
   console.log("clicked")
   var gameId = $(this).attr('id');
@@ -115,6 +145,7 @@ $(document).on('click', '.buttons', function () {
   var gamePathSteam = "C:/Program Files (x86)/Steam/steamapps/common/" + gameName + "/" + gameId;
   var gamePathOrigin = "C:/Program Files (x86)/Origin Games/" + gameName + "/" + gameId;
 
+  //checks if game is from Origin or Steam
   if (fs.existsSync(gamePathSteam)) {
     runExe(gamePathSteam);
   } else if (fs.existsSync(gamePathOrigin)) {
