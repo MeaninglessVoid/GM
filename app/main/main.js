2d2622518f591ba4bc6c8cd73d4e8ab32a62602f
Samuel Dubner <dubnersamuel@yahoo.com>
2017-11-01 22:08:32 -0700

    fix PUBG and icons

--- app/main/main.js
+++ app/main/main.js
@@ -1,5 +1,8 @@
+const remote = require('electron').remote;
 const fs = require('fs');
+const child = require('child_process').execFile;
 const iconExtractor = require('icon-extractor');
+const app = remote.app;
 
 addGame = (file, gameIcons) => {
   var gameDiv = $(document.createElement('div'));
@@ -11,16 +14,33 @@ addGame = (file, gameIcons) => {
   gameIcon.addClass('game-icon');
   gameTitle.addClass('game-title');
   playButton.addClass('buttons');
-  gameTitle.html(file);
+  if (file == "PUBG/TslGame/Binaries/Win64") {
+    gameTitle.html("PUBG");
+  } else {
+    gameTitle.html(file);
+  }
   console.log(gameIcons);
-  var iconPath = '../icons/' + gameIcons[0].substring(0, gameIcons[0].length - 4) + '.png';
-  gameIcon.attr('src', iconPath);
+  var iconPath = app.getPath("appData") + "/gm/icons/" + gameIcons[0].substring(0, gameIcons[0].length - 4);
   playButton.val('Play Game');
+  gameIcon.attr('src', iconPath + ".png");
   playButton.attr('type', 'button')
+  playButton.attr('id', gameIcons[0]);
+  playButton.attr('gameName', file);
   $('.game-container').append(gameDiv);
   $(gameDiv).append(gameIcon);
   $(gameDiv).append(gameTitle);
   $(gameDiv).append(playButton);
+}
+
+function runExe(executablePath) {
+  child(executablePath, function (err, data) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(data.toString());
+  });
 }
 
 const steam = "C:/Program Files (x86)/Steam/steamapps/common/";
@@ -28,8 +48,15 @@ const origin = "C:/Program Files (x86)/Origin Games/";
 
 let gameIcons = {};
 
+if (!fs.existsSync(app.getPath("appData") + "/gm/icons/")) {
+  fs.mkdirSync(app.getPath("appData") + "/gm/icons/");
+}
+
 fs.readdirSync(steam).forEach(file => {
   let pictureName;
+  if (file == "PUBG") {
+    file = file + "/TslGame/Binaries/Win64";
+  }
   gameIcons[file] = [];
   fs.readdirSync(steam + file).forEach(picture => {
     if (picture.substring(picture.length - 4, picture.length) == '.exe') {
@@ -60,11 +87,11 @@ for (let game in gameIcons) {
 }
 
 iconExtractor.emitter.on('icon', function (data) {
-  // console.log('Here is my context: ' + data.Context);
-  // console.log('Here is the path it was for: ' + data.Path);
   var icon = data.Base64ImageData;
 
-  fs.writeFile("./app/icons/" + data.Context.substring(0, data.Context.length - 4) + '.png', icon, 'base64', (err) => {
+  console.log(app.getPath("appData") + "/gm/" + data.Context.substring(0, data.Context.length - 4) + '.png');
+
+  fs.writeFile(app.getPath("appData") + "/gm/icons/" + data.Context.substring(0, data.Context.length - 4) + '.png', icon, 'base64', (err) => {
     console.error();
   });
 
@@ -76,4 +103,20 @@ $("#search-text").on("keyup", function () {
     var s = $(this).text().toLowerCase();
     $(this).closest('.game')[s.indexOf(g) !== -1 ? 'show' : 'hide']();
   });
+
 })
+
+$(document).on('click', '.buttons', function () {
+  console.log("clicked")
+  var gameId = $(this).attr('id');
+  var gameName = $(this).attr('gameName');
+  var gamePathSteam = "C:/Program Files (x86)/Steam/steamapps/common/" + gameName + "/" + gameId;
+  var gamePathOrigin = "C:/Program Files (x86)/Origin Games/" + gameName + "/" + gameId;
+
+  if (fs.existsSync(gamePathSteam)) {
+    runExe(gamePathSteam);
+  } else if (fs.existsSync(gamePathOrigin)) {
+    runExe(gamePathOrigin);
+  }
+
+});
