3ef72edeaec6ae7b0d0b456bdc169bdfcac6a752
SamuelDub <dubnersamuel@yahoo.com>
2017-11-16 09:30:07 -0800

    setting up home, and settings

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -1,3 +1,3 @@
 $('#save-button').on('click', function() {
-    console.log('Error: cannot save yet, please wait for future versions')
+    alert('Error: cannot save yet, please wait for future versions')
 })
